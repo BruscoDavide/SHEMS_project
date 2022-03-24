ed34e01853503c9c3eb1b2049a4289aa81908b32
BruscoDavide <80892333+BruscoDavide@users.noreply.github.com>
2022-03-24 12:40:41 +0100

    Multi-threading

---
+++ SHEMS_venv/SHEMS_project/Utilities/timer.py
@@ -0,0 +1,24 @@
+from threading import Timer
+
+class perpetualTimer():
+    def __init__(self,t,hFunction):
+        """Recursiove perpetual timer
+
+        Args:
+            t (int): time interval between 2 callbakcs [s]
+            hFunction (function): callback function
+        """
+        self.t=t
+        self.hFunction = hFunction
+        self.thread = Timer(self.t,self.handle_function)
+
+    def handle_function(self):
+        self.hFunction()
+        self.thread = Timer(self.t,self.handle_function)
+        self.thread.start()
+
+    def start(self):
+        self.thread.start()
+
+    def cancel(self):
+        self.thread.cancel()
--- SHEMS_venv/SHEMS_project/main.py
+++ SHEMS_venv/SHEMS_project/main.py
@@ -1,1 +1,35 @@
-# test 
+#opzione 1, web server, prosumer_comm, local GUI scrivono su file json che vado a leggere
+# MAIN CON TRE CICLI IN MULTITHREAD CHE GIRANO E LEGGONO FILE QUANDO C'è LA CONDIZIONE RICHIAMO DELLE CALLBACK
+import json
+
+
+from Utilities.timer import perpetualTimer
+
+def reading_json_callback():
+    fp = open('test.json', 'r')
+    file = json.load(fp)
+    fp.close()
+    print(file)
+
+def reading_json_callback2():
+    fp = open('test2.json', 'r')
+    file = json.load(fp)
+    fp.close()
+    print(file)
+
+def reading_json_callback3():
+    fp = open('test3.json', 'r')
+    file = json.load(fp)
+    fp.close()
+    print(file)
+
+if __name__ == '__main__':
+    t = 1
+    test = perpetualTimer(t, reading_json_callback)
+    test.start()
+
+    test = perpetualTimer(t, reading_json_callback2)
+    test.start()
+
+    test = perpetualTimer(t, reading_json_callback3)
+    test.start()
